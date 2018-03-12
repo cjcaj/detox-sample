8a2b11bd22a6e1b81bd5388f21852bb3f39e35d4
Carlo C <carlo.cajucom@gmail.com>
2018-03-12 10:20:42 -0700

    [App] update

--- App.js
+++ App.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { ActivityIndicator, Button, StyleSheet, Text, View } from 'react-native'
 
 export default class App extends React.Component {
+  state = {
+    taps: 0,
+    fact: null,
+    factLoading: false,
+  }
+
+  tap = () => {
+    this.setState(prevState => ({ taps: prevState.taps + 1 }))
+  }
+
+  getCatFact = () => {
+    this.setState({ factLoading: true })
+    fetch('https://catfact.ninja/fact')
+      .then(response => response.json())
+      .then(responseJson => this.setState({ fact: responseJson.fact, factLoading: false }))
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.taps !== this.state.taps && this.state.taps % 5 === 0) {
+      this.getCatFact()
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Button title="Click here" onPress={this.tap} testID="button-tap" />
+
+        {this.state.taps % 2 === 1 && (
+          <Text testID="text-taps">{this.state.taps}</Text>
+        )}
+
+        {this.state.fact && <Text testID="text-fact">{this.state.fact}</Text>}
+        {this.state.factLoading && <ActivityIndicator />}
       </View>
-    );
+    )
   }
 }
 
@@ -19,5 +47,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    margin: 20,
   },
-});
+})
