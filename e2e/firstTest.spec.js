52a8c1bd0a9cc415f6c0b482fc03158ef3322962
Carlo C <carlo.cajucom@gmail.com>
2018-03-12 11:42:01 -0700

    [spec] update with helper

--- e2e/firstTest.spec.js
+++ e2e/firstTest.spec.js
@@ -1,19 +1,34 @@
+const { reloadApp } = require('detox-expo-helpers')
+const { takeScreenshot } = require ('./helpers')
+
 describe('Example', () => {
   beforeEach(async () => {
-    await device.reloadReactNative();
-  });
-  
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('welcome'))).toBeVisible();
-  });
-  
-  it('should show hello screen after tap', async () => {
-    await element(by.id('hello_button')).tap();
-    await expect(element(by.text('Hello!!!'))).toBeVisible();
-  });
-  
-  it('should show world screen after tap', async () => {
-    await element(by.id('world_button')).tap();
-    await expect(element(by.text('World!!!'))).toBeVisible();
-  });
+    await reloadApp()
+  })
+
+  afterEach(async () => {
+    takeScreenshot()
+  })
+
+  it('should only show button', async () => {
+    await expect(element(by.id('button-tap'))).toBeVisible()
+  })
+
+  it('should show tap 1 and tap 3', async () => {
+    await element(by.id('button-tap')).tap()
+    await expect(element(by.id('text-taps'))).toBeVisible()
+    await element(by.id('button-tap')).tap()
+    await expect(element(by.id('text-taps'))).toBeNotVisible()
+    await element(by.id('button-tap')).tap()
+    await expect(element(by.id('text-taps'))).toBeVisible()
+  })
+
+  it('should show cat fact on tap 5', async () => {
+    await element(by.id('button-tap')).tap()
+    await element(by.id('button-tap')).tap()
+    await element(by.id('button-tap')).tap()
+    await element(by.id('button-tap')).tap()
+    await element(by.id('button-tap')).tap()
+    await expect(element(by.id('text-fact'))).toBeVisible()
+  })
 })
